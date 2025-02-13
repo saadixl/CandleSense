e1d219e49744b2c7b0f7212f31e2f6c0f983aaaa
Masudul Haque <saadixl@outlook.com>
2025-02-14 00:12:53 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -26,22 +26,17 @@ sample_images_file_paths = [os.path.join(sample_images_dir, f) for f in os.listd
 # Generates image embeddings
 def get_image_embedding(image_path):
     image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-    
     with torch.no_grad():
         embedding = model.encode_image(image)
-
     return embedding.cpu().numpy().flatten()
 
 # Finds the closest matching chart based on cosine similarity
 def get_closest_match(sample_image_path, training_embeddings):
     sample_embedding = get_image_embedding(sample_image_path)
-
     # Compute cosine similarity
     similarities = cosine_similarity([sample_embedding], training_embeddings)
-
     # Get index of the most similar chart
     closest_index = np.argmax(similarities)
-
     return training_images_file_paths[closest_index], similarities[0][closest_index]
 
 def interpret_sample_file(file_name, training_embeddings):
@@ -81,11 +76,14 @@ def main():
         print_line()
         print("\nGetting candle stick chart for " + ticker)
         try:
+            # Get Stock data
             stock = yf.Ticker(ticker)
             df = stock.history(period="30d")
             new_file_name = ticker +".png"
+            # Create a candle list chart image from the data
             mpf.plot(df, type="candle", style="charles", volume=True, savefig="data/sample_images/" + new_file_name)
             print("Candle stick chart " + new_file_name + " saved.\nInterpretting new chart...")
+            # Interpret the newly created chart
             interpret_sample_file(new_file_name, training_embeddings)
             print_line()
         except:
