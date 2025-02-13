facfce0d8dce7a4ca3d982dffafffcf10d19cd13
Masudul Haque <saadixl@outlook.com>
2025-02-14 00:45:40 +0800

    Render closest match chart as well

--- src/main.py
+++ src/main.py
@@ -44,11 +44,16 @@ def interpret_sample_file(file_name, training_embeddings):
     sample_image_path = os.path.join(sample_images_dir, file_name)
     closest_match, similarity_score = get_closest_match(sample_image_path, training_embeddings)
     closest_match_filename = os.path.basename(closest_match)
+    closest_match_path = os.path.join(training_images_dir, closest_match_filename)
     price_movement_prediction = 'down' if closest_match_filename.startswith('d') else 'up'
     closest_match_chart_type = os.path.splitext(closest_match_filename)[0][2:]
+    
     sample_image_filename = os.path.basename(sample_image_path)
     formatted_percentage_str = f"{similarity_score*100:.2f}%"
-    print(f"\nSample file\t: {sample_image_filename}\nClosest match\t: {closest_match_filename}\nSimilarity score: {formatted_percentage_str}\n\033[1m{closest_match_chart_type} indicates price may go {price_movement_prediction}\033[0m\n")
+    print(f"\nSample file\t: {sample_image_filename}\nClosest match\t: {closest_match_filename}")
+    display_image(closest_match_path)
+    print(f"Similarity score: {formatted_percentage_str}\n\033[1m{closest_match_chart_type} indicates price may go {price_movement_prediction}\033[0m\n")
+
 
 def delete_all_sample_images():
     for file_name in os.listdir(sample_images_dir):
@@ -60,7 +65,10 @@ def print_line():
     print("--------------------------------------------------------")
 
 def display_image(image_path):
-    subprocess.run(["imgcat", image_path])
+    try:
+        subprocess.run(["imgcat", image_path])
+    except:
+        print("Can't display candle chart image. Please use iTerm.")
 
 def main():
     # Generating embeddings for training images
@@ -87,10 +95,7 @@ def main():
             # Create a candle list chart image from the data
             new_file_path = "data/sample_images/" + new_file_name
             mpf.plot(df, type="candle", style="charles", volume=True, savefig=new_file_path)
-            try:
-                display_image(new_file_path)
-            except:
-                print("Can't display candle chart image. Please use iTerm.")
+            display_image(new_file_path)
             print("New candle stick chart saved to " + new_file_path + "\nInterpretting new chart...")
             # Interpret the newly created chart
             interpret_sample_file(new_file_name, training_embeddings)
